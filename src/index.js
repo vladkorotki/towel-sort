b863df3b355ae86c8408cd2be8a332c884d3fa06
Vlad Korotki <vladkorotki92@gmail.com>
2021-08-23 23:01:23 +0300

    feat: Task Tower-sort is ready

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix) return [];
+
+  let result = matrix;
+
+  result = result.map((item, index) => {
+
+    if (index % 2) item.reverse();
+
+    return item;
+
+  }).join(',').split(',');
+
+  if (result.includes('')) return [];
+
+  return result;
 }
